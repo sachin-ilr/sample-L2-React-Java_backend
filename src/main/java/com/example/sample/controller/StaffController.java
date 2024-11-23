c0eb3969763f8b05489557c7af5538d173bf1842
sachin ilr <sachu.ilrad@gmail.com>
2024-11-23 20:09:52 +0530

    pom changes

--- src/main/java/com/example/sample/controller/StaffController.java
+++ src/main/java/com/example/sample/controller/StaffController.java
@@ -1,12 +1,20 @@
 package com.example.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sample.entity.Staff;
 import com.example.sample.services.StaffServices;
-import org.springframework.data.domain.Page;
 
 @RestController
 @RequestMapping("/staff")
